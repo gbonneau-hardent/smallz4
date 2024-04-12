8352db04e386a301e353339205a2af1def3772cb
mrvallee <mrvallee@rambus.com>
2024-04-12 13:15:23 -0700

    add get cell match

--- DPIWrapper.cpp
+++ DPIWrapper.cpp
@@ -66,8 +66,56 @@ extern "C" uint32_t PushData(unsigned char data[NB_BYTE], chandle compression_mo
     return 0;
 }
 
-/// @brief Get the list of match
+/// @brief Get the list of cell match (one call per clk)
 /// @param match_list match list of all the cell
+/// @param compression_model handle to the compression model
+/// @return status of casting (0 success and 1 error)
+extern "C" uint32_t GetCellMatch(Matchstruct match_cell_list [CHUNKSIZE], chandle compression_model)
+{
+    match_detection_model *match_detection = dynamic_cast<match_detection_model *>(reinterpret_cast<match_detection_model *>(compression_model));
+    // Matchstruct match_cell_list [CHUNKSIZE];
+
+    if (match_detection == nullptr) {
+        return 1;
+    }
+
+
+    for (size_t cell = 0; cell < NB_CELL; cell++)
+    {
+        for (size_t pos = 0; pos < NB_BYTE; pos++)
+        {
+            uint32_t pos1d = pos+(cell*NB_BYTE);
+            match_cell_list[pos1d] = match_detection->match_cell[cell].getMatch(pos);
+
+            if (match_cell_list[pos1d].valid)
+            {
+                printf("GetCellMatch: match_cell_list[%u].valid = %u\n", pos1d, match_cell_list[pos1d].valid);
+                printf("GetCellMatch: match_cell_list[%u].offset = %u\n", pos1d, match_cell_list[pos1d].offset);
+                printf("GetCellMatch: match_cell_list[%u].length = %u\n", pos1d, match_cell_list[pos1d].length);
+                printf("GetCellMatch: match_cell_list[%u].large_counter = %u\n", pos1d, match_cell_list[pos1d].large_counter);
+            }
+        }
+    }
+
+    // *match_list = match_cell_list;
+
+    // for (size_t idx = 0; idx < CHUNKSIZE; idx++)
+    // {
+    //     if ((*match_list)[idx].valid)
+    //     {
+    //         printf("GetCellMatch: match_list[%llu].valid = %u\n", idx, (*match_list)[idx].valid);
+    //         printf("GetCellMatch: match_list[%llu].offset = %u\n", idx, (*match_list)[idx].offset);
+    //         printf("GetCellMatch: match_list[%llu].length = %u\n", idx, (*match_list)[idx].length);
+    //         printf("GetCellMatch: match_list[%llu].large_counter = %u\n", idx, (*match_list)[idx].large_counter);
+    //     }
+
+    // }
+
+    return 0;
+}
+
+/// @brief Get the list of match merged (one call per chunk)
+/// @param match_list match list of all the cell merged
 /// @param compression_model handle to the compression model
 /// @return status of casting (0 success and 1 error)
 extern "C" uint32_t GetMatch(Matchstruct** match_list, chandle compression_model)
@@ -82,12 +130,12 @@ extern "C" uint32_t GetMatch(Matchstruct** match_list, chandle compression_model
 
     // for (size_t i = 0; i < CHUNKSIZE; i++)
     // {
-    //     if (match_list[i].valid)
+    //     if (*match_list[i].valid)
     //     {
-    //         printf("MRV: match_list[%llu].valid = %u\n", i, match_list[i].valid);
-    //         printf("MRV: match_list[%llu].pos = %u\n", i, match_list[i].pos);
-    //         printf("MRV: match_list[%llu].offset = %u\n", i, match_list[i].offset);
-    //         printf("MRV: match_list[%llu].length = %u\n", i, match_list[i].length);
+    //         printf("GetMatch: match_list[%llu].valid = %u\n", i, *match_list[i].valid);
+    //         printf("GetMatch: match_list[%llu].offset = %u\n", i, *match_list[i].offset);
+    //         printf("GetMatch: match_list[%llu].length = %u\n", i, *match_list[i].length);
+    //         printf("GetMatch: match_list[%llu].large_counter = %u\n", i, *match_list[i].large_counter);
     //     }
 
     // }
--- DPIWrapper.h
+++ DPIWrapper.h
@@ -4,6 +4,7 @@
 #include <cstdint>
 #include <memory>
 #include <string.h>
+#include <assert.h>
 
 #ifndef chandle
 #define chandle void *
@@ -11,4 +12,5 @@
 
 extern "C" chandle InitCompression();
 extern "C" uint32_t PushData(unsigned char data[NB_BYTE], chandle compression_model);
+extern "C" uint32_t GetCellMatch(Matchstruct match_cell_list [CHUNKSIZE], chandle compression_model);
 extern "C" uint32_t GetMatch(Matchstruct** match_list, chandle compression_model);
